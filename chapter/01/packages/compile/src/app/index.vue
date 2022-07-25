e0244532f617234efef8ae7c4598d7bb987e9e7e
miniflycn <miniflycn@justany.net>
2022-07-25 21:21:22 +0800

    init

---
+++ chapter/01/packages/compile/src/app/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="v-counter">
+    <VText :text="num" />
+    <button class="v-btn" @click="click">点击加1</button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import VText from './text.vue';
+  const num = ref(0);
+  const click = () => {
+    num.value += 1;
+  }
+</script>
+
+<style>
+  .v-counter {
+    width: 200px;
+    margin: 20px auto;
+    padding: 10px;
+    color: #666666;
+    box-shadow: 0px 0px 9px #00000066;
+    text-align: center;
+  }
+  .v-counter .v-text {
+    font-size: 28px;
+    font-weight: bolder;
+  }
+  .v-counter .v-btn {
+    font-size: 20px;
+    padding: 0 10px;
+    height: 32px;
+    cursor: pointer;
+  }
+</style>
+
---
+++ chapter/01/packages/compile/src/app/text.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="v-text">{{ props.text }}</div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    text: Number
+  });
+</script>
+
+<style>
+.v-text {
+  font-size: 28px;
+  font-weight: bolder;
+}
+</style>
+
